fddabb0c0c2826181fa64bb6c0ea618bb16ce716
jcsilver3 <jcsilver3@gmail.com>
2024-12-06 14:15:24 -0500

    Update Simulation.swift
    Added license block.

--- VoterAgents/Simulation.swift
+++ VoterAgents/Simulation.swift
@@ -1,8 +1,21 @@
 //
-//  Sim.swift
+//  Agent.swift
 //  VoterAgents
+//  This file is part of the VoterAgents program, an Agent Based Model (ABM) simulation of misinformation perception.
+//  Copyright (C) 2024 John Silver (jcsilver3@gmail.com, jsilver9@gmu.edu)
 //
-//  Created by John Silver on 7/23/24.
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
 import Foundation
